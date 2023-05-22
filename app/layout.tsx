b2df7d0ba2e32cf870256094b2a0590e97d6afac
Claire_M <clairerodriguez79@yahoo.fr>
2023-05-22 13:52:14 +0200

    Install project + création navbar component et MyProfilePic component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
+import Navbar from './components/Navbar'
+import MyProfilePic from './components/MyProfilePic'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body  className="dark:bg-slate-800">
+        <Navbar />
+        <MyProfilePic />
+        {children}
+      </body>
     </html>
   )
 }
